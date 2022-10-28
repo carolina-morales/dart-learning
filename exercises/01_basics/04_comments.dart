2cd98b9280d6dc086cb25b3a3e33c1599a8f31ca
Carolina Morales <cdelacruz@asdeporte.com>
2022-10-28 14:51:24 -0500

    comments

---
+++ exercises/01_basics/04_comments.dart
@@ -0,0 +1,15 @@
+main() {
+  // this is a temp variable
+  int a = 10;
+
+  /* final people = [
+    // this is the first person
+    'Juan',
+    'Pedro',
+    'Sara'
+  ]; */
+}
+
+/// greeting function
+/// receive a [name] and a [message]
+greeting(String name, String message) {}
